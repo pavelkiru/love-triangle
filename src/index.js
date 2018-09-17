573eece1bad17659048e1aa5299ada7541baf92f
pavelkiru <patamt1036@mail.ru>
2018-09-17 17:20:53 +0300

    функция поиска любовного треугольника

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+
+  var count = 0;  
+  for (var i = 0; i < preferences.length; i++) { 
+      var a = preferences[i] - 1;
+      if (a == i) continue;
+      var b = preferences[a] - 1;
+      if (a == b) continue;
+      var c = preferences[b] - 1;
+      if (c == b) continue;
+      if (c == i) {
+          count++;
+      }
+   }
+   return count / 3;
 };
